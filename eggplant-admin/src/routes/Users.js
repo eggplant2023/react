b6f207dba8d946a184a9c11e6a81e493e4992e73
BURI966 <dumba966@gmail.com>
2023-03-20 16:39:59 +0900

    chatroom get test

--- eggplant-admin/src/routes/Users.js
+++ eggplant-admin/src/routes/Users.js
@@ -1,7 +1,17 @@
-const Users = () => (
+const Users = () => {
+    
+    useEffect(() => {
+        PostingService.getChatroom().then((res)=> {
+                console.log(res.data)
+            }
+        )
+           
+   },[]);
+
+    return (
     <div>
         <h1> Users </h1>
     </div>
-);
+);}
 
 export default Users;
