819ad7642dff35be48b8777537a44933fef06f33
BURI966 <dumba966@gmail.com>
2023-04-12 16:55:18 +0900

    socket client & stomp

--- eggplant-admin/src/components/GetChatList.js
+++ eggplant-admin/src/components/GetChatList.js
@@ -39,7 +39,7 @@ const GetChatList = ({ roomnumber, close }) => {
                 msg[i] = {message: res.data[i].cht_text,
                     authorId: res.data[i].cht_member,
                     author: res.data[i].cht_membe,
-                    timestamp: cht_time
+                    timestamp: res.data[i].cht_time
                 }
             }
             setChatList(msg)
