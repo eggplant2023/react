26e4d6d1d42bc836f1140d34c23d21ada3c9ba6d
BURI966 <dumba966@gmail.com>
2023-05-25 10:39:02 +0900

    nearmap

--- eggplant-admin/src/mobiles/Nearmap.js
+++ eggplant-admin/src/mobiles/Nearmap.js
@@ -7,14 +7,14 @@ const { kakao } = window;
 
 const NearMap = () => {
 
-    var map 
+    var map
 
     const [lat, setLat] = useState(37.582286794056294)
     const [lon, setLon] = useState(127.00981565163072)
     const [positions, setPositions] = useState([])
     const [searchParams, setSearchParams] = useSearchParams();
     const post = searchParams.get('post'); // postnum 값 변수에 저장
-
+    const [level, setLevel] = useState(1);
     function setScreenSize() {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -23,13 +23,13 @@ const NearMap = () => {
     const setLocations = () => {
         let temp = []
         let data = []
-        PostingService.getNearLocation(lon, lat, 10).then((res) => {
+        PostingService.getNearLocation(lon, lat, level*10).then((res) => {
             data = res.data
 
-            for(var i = 0; i < data.length; i++ ){
+            for (var i = 0; i < data.length; i++) {
                 console.log(data[i])
                 temp[i] = {
-                    num : data[i].post_num,
+                    num: data[i].post_num,
                     title: data[i].post_title,
                     img: data[i].pictureURL,
                     price: data[i].price,
@@ -37,7 +37,7 @@ const NearMap = () => {
                     latlng: new kakao.maps.LatLng(data[i].location.latitude.toFixed(6), data[i].location.longitude.toFixed(6))
                 }
             }
-            
+
             temp[temp.length] = {
                 num: "",
                 title: '내 위치',
@@ -76,39 +76,47 @@ const NearMap = () => {
                 position: pos.latlng, // 마커를 표시할 위치
                 title: pos.title
             });
-        
+
             // content HTMLElement 생성
             var content = document.createElement('div');
             content.classList.add('test');
             var info = document.createElement('span');
             info.appendChild(document.createTextNode(pos.title));
             content.appendChild(info);
-        
+
             var closeBtn = document.createElement('button');
             closeBtn.appendChild(document.createTextNode('닫기'));
 
             var goto = document.createElement('button');
             goto.appendChild(document.createTextNode('이동'));
 
-            // 닫기 이벤트 추가
+            var zoomControl = new kakao.maps.ZoomControl();
+            map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
+            kakao.maps.event.addListener(map, 'zoom_changed', function() {        
+    
+                // 지도의 현재 레벨을 얻어옵니다
+                setLevel(map.getLevel());
+            });
 
             closeBtn.onclick = () => {
                 overlay.setMap(null);
             };
-            
+
 
             goto.onclick = () => {
                 sendMessage(pos.num);
             };
             content.appendChild(closeBtn);
             content.appendChild(goto);
-        
+
             // customoverlay 생성, 이때 map을 선언하지 않으면 지도위에 올라가지 않습니다.
             var overlay = new kakao.maps.CustomOverlay({
                 position: pos.latlng,
                 content: content
             });
-        
+
             // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
             kakao.maps.event.addListener(marker, 'click', () => {
                 overlay.setMap(map);
@@ -119,7 +127,7 @@ const NearMap = () => {
     useEffect(() => {
         setScreenSize();
         setLocations();
-    },[]);
+    }, []);
 
     useEffect(() => {
         createMaps();
@@ -129,7 +137,9 @@ const NearMap = () => {
         <div className="mappage">
 
             <div id="map" style={{ width: "500px", height: "500px" }} />
+            <div style={{ width: "500px", height: "100px" }}>
 
+            </div>
         </div>
 
     );
