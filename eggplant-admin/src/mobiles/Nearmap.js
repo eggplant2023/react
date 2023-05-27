936569de52ee11de70f37f92dbceb8aa38dac17b
BURI966 <dumba966@gmail.com>
2023-05-27 18:11:01 +0900

    post style

--- eggplant-admin/src/mobiles/Nearmap.js
+++ eggplant-admin/src/mobiles/Nearmap.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import PostingService from '../services/PostingService';
 import { useLocation, useSearchParams } from "react-router-dom"
-
-
+import boogie from '../img/boogie.png'
 const { kakao } = window;
 
 const NearMap = () => {
 
     var map
 
-    const [lat, setLat] = useState(37.582286794056294)
-    const [lon, setLon] = useState(127.00981565163072)
     const [positions, setPositions] = useState([])
     const [searchParams, setSearchParams] = useSearchParams();
-    const post = searchParams.get('post'); // postnum 값 변수에 저장
+    const lat = searchParams.get('lat')
+    const lon = searchParams.get('lon');
     const [level, setLevel] = useState(1);
     function setScreenSize() {
         let vh = window.innerHeight * 0.01;
@@ -23,7 +21,7 @@ const NearMap = () => {
     const setLocations = () => {
         let temp = []
         let data = []
-        PostingService.getNearLocation(lon, lat, level*10).then((res) => {
+        PostingService.getNearLocation(lon, lat, level * 10).then((res) => {
             data = res.data
 
             for (var i = 0; i < data.length; i++) {
@@ -73,77 +71,99 @@ const NearMap = () => {
         map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 
-        kakao.maps.event.addListener(map, 'zoom_changed', function() {        
+        kakao.maps.event.addListener(map, 'zoom_changed', function () {
 
             // 지도의 현재 레벨을 얻어옵니다
             setLevel(map.getLevel());
         });
 
         temp.forEach(function (pos) {
-            // 마커를 생성합니다
-            var marker = new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: pos.latlng, // 마커를 표시할 위치
-                title: pos.title
-            });
+
+
+            if (pos.title == "내 위치") {
+                var imageSrc = boogie,
+                    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+                    imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+                // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+                var marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: pos.latlng, // 마커를 표시할 위치
+                    image: markerImage,
+                });
+            }
+
+            else {
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: pos.latlng, // 마커를 표시할 위치
+                    title: pos.title
+                });
+            }
 
             // content HTMLElement 생성
-            if(pos.title != '내 위치'){
-            var content = document.createElement('div');
-            content.classList.add('ovcontent');
+            if (pos.title != '내 위치') {
+                var content = document.createElement('div');
+                content.classList.add('ovcontent');
 
-            var titlediv = document.createElement('div');
-            var title = document.createElement('span');
-            var closeBtn = document.createElement('button');
-            closeBtn.appendChild(document.createTextNode('X'));
-            title.appendChild(document.createTextNode(pos.title+"       "));
-            titlediv.appendChild(title);
-            titlediv.appendChild(closeBtn);
-            content.appendChild(titlediv);
-            titlediv.classList.add('ovtitle');
+                var titlediv = document.createElement('div');
+                var title = document.createElement('span');
+                var closeBtn = document.createElement('button');
+                closeBtn.appendChild(document.createTextNode('X'));
+                title.appendChild(document.createTextNode(pos.title + "       "));
+                titlediv.appendChild(title);
+                titlediv.appendChild(closeBtn);
+                content.appendChild(titlediv);
+                titlediv.classList.add('ovtitle');
 
-            var image = document.createElement('img');
-            image.setAttribute("src",pos.img);
-            image.setAttribute("width","150px");
-            image.setAttribute("height","150px");
-            content.appendChild(image);
-            image.classList.add('ovimage');
+                var image = document.createElement('img');
+                image.setAttribute("src", pos.img);
+                image.setAttribute("width", "150px");
+                image.setAttribute("height", "150px");
+                content.appendChild(image);
+                image.classList.add('ovimage');
 
-            var controlldiv = document.createElement('div');
-
-
-
-            // var goto = document.createElement('button');
-            // goto.appendChild(document.createTextNode('이동'));
-
-            // var buttondiv = document.createElement('div');
-            // buttondiv.classList.add('ovbuttondiv');
-
-            closeBtn.onclick = () => {
-                overlay.setMap(null);
-            };
+                var controlldiv = document.createElement('div');
 
 
-            image.onclick = () => {
-                sendMessage(pos.num);
-            };
 
-            // controlldiv.appendChild(buttondiv);
-            // controlldiv.appendChild(goto);
-            // controlldiv.classList.add('ovcontrol');
+                // var goto = document.createElement('button');
+                // goto.appendChild(document.createTextNode('이동'));
+
+                // var buttondiv = document.createElement('div');
+                // buttondiv.classList.add('ovbuttondiv');
+
+                closeBtn.onclick = () => {
+                    overlay.setMap(null);
+                };
 
 
-            // customoverlay 생성, 이때 map을 선언하지 않으면 지도위에 올라가지 않습니다.
-            var overlay = new kakao.maps.CustomOverlay({
-                position: pos.latlng,
-                content: content
-            });
+                image.onclick = () => {
+                    sendMessage(pos.num);
+                };
 
-            // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-            kakao.maps.event.addListener(marker, 'click', () => {
-                overlay.setMap(map);
-            });
-        }
+                // controlldiv.appendChild(buttondiv);
+                // controlldiv.appendChild(goto);
+                // controlldiv.classList.add('ovcontrol');
+
+
+                // customoverlay 생성, 이때 map을 선언하지 않으면 지도위에 올라가지 않습니다.
+                var overlay = new kakao.maps.CustomOverlay({
+                    position: pos.latlng,
+                    content: content
+                });
+
+                // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+                kakao.maps.event.addListener(marker, 'click', () => {
+                    overlay.setMap(map);
+                });
+            }
+
+
+
+
         });
     }
 
