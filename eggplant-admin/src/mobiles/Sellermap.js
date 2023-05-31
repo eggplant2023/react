f343677112218849fd0ac943c41ac2aa84a5fd0d
BURI966 <dumba966@gmail.com>
2023-05-31 12:41:27 +0900

    SH map'

--- eggplant-admin/src/mobiles/Sellermap.js
+++ eggplant-admin/src/mobiles/Sellermap.js
@@ -232,7 +232,7 @@ const SellerMap = () => {
         temp.forEach(function (pos) {
 
 
-            if (postMessage.post_num == num) {
+            if (pos.post_num == num) {
                 var imageSrc = sellerpin,
                     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
                     imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
